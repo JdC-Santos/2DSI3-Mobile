febdbb1ff40974eaf4bc32096f2d7d89d7003cfc
Jhonatancs <jdc_santos@outlook.com>
2019-07-30 08:27:28 +0900

    adicionando plugins no app

--- www/components/functions.js
+++ www/components/functions.js
@@ -1,2 +1,6 @@
 // This is a JavaScript file
-
+$(document).on('click','#alerta',function(){
+  navigator.notification.alert("Voce tem "+qtdMsg+" Mensagens");
+  navigator.notification.beep(2);
+  navigator.vibrate([1000, 500, 1000, 500, 1000]);
+});
